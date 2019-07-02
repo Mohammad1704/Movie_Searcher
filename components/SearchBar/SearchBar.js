cde6618a7dd5f720af0c55574863fa091125f901
Mhammad1704 <natgeo1416@hotmail.com>
2019-07-02 17:13:13 +0300

    working Base
    its work agine

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {TextInput, TouchableOpacity, View} from 'react-native';
+// import {FontAwesome} from '@expo/vector-icons';
+import {API_KEY} from '../../constants/api.js';
+
+import {connect} from 'react-redux';
+import {searchResults} from "../../actions";
+
+const styles = require('./SearchBarStyles');
+const cx = '012040760514751621405:3qggxrt9tk8';
+const apiURL = 'https://www.googleapis.com/customsearch/v1'
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+  }
+  searchOnMedium = () => {
+    let URL = apiURL + '?key=' + API_KEY + '&cx=' + cx + '&q=' + this.state.searchTerm;
+    fetch(URL, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then((response) => {
+      response.json().then((data) => {
+        this.props.searchResults(data.items);
+      })
+    }).catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          placeholder='Enter your search terms'
+          style={styles.textInputSearch}
+          underlineColorAndroid={'transparent'}
+          onChangeText={(searchTerm) => this.setState({ searchTerm })}
+          value={this.state.searchTerm}
+        />
+        <TouchableOpacity
+          style={styles.textSearchButton}
+          onPress={() => this.searchOnMedium()}
+        >
+          {/* <FontAwesome name="search" size={16} color="#000"/> */}
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default connect(null, {searchResults})(SearchBar);
---
+++ components/SearchResults/SearchResults.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Linking, Text, TouchableOpacity, View} from 'react-native';
+
+import {connect} from 'react-redux';
+
+const styles = require('./SearchResultsStyles');
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <View style={styles.searchResultsContainer}>     
+        {this.props.results.map((result, key) => (
+          <TouchableOpacity 
+            onPress={() => {Linking.openURL(result.formattedUrl) }}
+          >
+            <Text key={key}>{result.title}</Text>
+          </TouchableOpacity>
+         ))}
+       </View>
+    )
+  }}
+  function mapStateToProps(state) {
+    return {
+      results: state.results
+    };
+  }
+  export default connect(mapStateToProps, null)(SearchResults);
